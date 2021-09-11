0a27032e5db114158ff6b4dc20611e8d7a4fa1f9
wugensheng <82647680+wugensheng-code@users.noreply.github.com>
2021-09-11 16:52:58 +0800

    Modify the path of xpack-riscv-none-embed-gcc in config_env.py

--- config_env.py
+++ config_env.py
@@ -11,7 +11,7 @@ def increase_env():
         export PATH=${JAVA_HOME}/bin:$PATH
         export PATH=/rt-thread/gcc-arm-none-eabi-6-2017-q2-update/bin:$PATH
         export PATH=/rt-thread/sdk-toolchain-RISC-V-GCC-riscv64-unknown-elf-gcc-8.3.0-linux-x64/bin:$PATH
-        export PATH=/rt-thread/xpack-riscv-none-embed-gcc-8.3.0-2.3-linux-x64/bin:$PATH
+        export PATH=/rt-thread/xpack-riscv-none-embed-gcc-8.3.0-2.3/bin:$PATH
         """
     env = textwrap.dedent(env)
     with open(env_path, 'a') as f:
